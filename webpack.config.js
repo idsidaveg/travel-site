bfc65bf4a28468e3d2d4fc87f956ed33b200b36f
idsidaveg <idsidaveg@gmail.com>
2021-10-21 16:42:33 -0600

    Completed Hero Block and Large Content Block

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
@@ -10,6 +12,15 @@ module.exports = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html');
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    },
     resolve: {
         alias: {
 
@@ -17,7 +28,6 @@ module.exports = {
         extensions: ['.ts', '.js'],
     },
     mode: 'development',
-    watch: true,
     module: {
         rules: [
             {
