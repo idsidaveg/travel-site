ae4a66fc2779ebf3db594832e5a76879eef279db
idsidaveg <idsidaveg@gmail.com>
2021-10-26 16:23:17 -0600

    Completed the reveal on scroll behavior

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,8 @@ module.exports = {
         path: path.resolve(__dirname, 'app')
     },
     devServer: {
-        before: function(app, server) {
-            server._watch('./app/**/*.html');
-        },
-        contentBase: path.join(__dirname, 'app'),
+        watchFiles: ['./app/**/*.html'],
+        static: path.join(__dirname, 'app'),
         hot: true,
         port: 3000,
         host: '0.0.0.0'
