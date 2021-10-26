b5e4b956047ad520305f70da00f9c3603b6f2d31
idsidaveg <idsidaveg@gmail.com>
2021-10-26 17:41:01 -0600

    Finished sticky header changes - menu selection, scrolling, etc.)

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,12 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/Sticky-Header';
 
+let stickHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+
 
 let mobileMenu = new MobileMenu();
 
