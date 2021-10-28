13068bed4a02f51d5f745fd6876740860182371c
idsidaveg <idsidaveg@gmail.com>
2021-10-28 10:35:53 -0600

    Updating so CORS functionality is not impacted, did not work

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,18 @@
 exports.handler = function(event, context, callback) {
+    const headers = {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Headers" : "Content-Type"
+    }
+
+    if (event.httpMethod !== "POST"){
+        return callback(null, {
+            statusCode = 200,
+            headers,
+            body: "This is not a POST request"
+        });
+    };
+
+
     // define the HTML that will be injected at the boot of the page
     const secretContent = `
     <h3>Welcome to the secret area</h3>
@@ -12,7 +26,7 @@ exports.handler = function(event, context, callback) {
         body = {};
     }
 
-    if (body.password == "javascript") {
+    if (body.password == "JavaScript") {
         callback(null, {
             statusCode: 200,
             body: secretContent
