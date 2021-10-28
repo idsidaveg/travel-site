e69fb925401a5dedd950163f176d43e3a6826747
idsidaveg <idsidaveg@gmail.com>
2021-10-28 08:46:11 -0600

    Updated cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,27 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the super secret area"
-    });
+    // define the HTML that will be injected at the boot of the page
+    const secretContent = `
+    <h3>Welcome to the secret area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+    `;
+    let body;
+
+    if (event.body){
+        body = JSON.parse(event.body);
+    } else {
+        body = {};
+    }
+
+    if (body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        });
+
+    } else {
+        callback(null, {
+            statusCode: 401
+        });
+    }
+
 };
